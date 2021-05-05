cd39e8022e918c51c8e50e4801da3f98f46651da
Daniel Orduña <danordua@gmail.com>
2021-05-05 10:17:08 -0500

    Cambios después de la clase del martes

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const hiden = document.querySelector('.hiden');
-const hidenBtn = document.querySelector("#hiden-btn")
+const hidenBtn = document.querySelector("#hiden-btn");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const quizBtns = document.querySelectorAll(".btn-quiz");
+const quizText = document.querySelector("#qText");
 
+let resultValues =  new Array();
+
+console.log(resultValues);
 
 const startQuiz = () => {
 hiden.style.display = "none";
 };
 
 hidenBtn.addEventListener('click', startQuiz);
+
+(function quiz(){
+    quizBtns.forEach(btn => {
+        btn.addEventListener('click', () => {
+            quizText.innerText = "¿Dónde te gustaría estar?";
+            let val = btn.getAttribute("value");
+            resultValues.push(val);
+            console.log(resultValues);
+        })
+    })
+})();
+
+
