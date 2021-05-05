762bc10f6d2426ca853e4804a5b0d5d048f16c8c
Daniel Orduña <danordua@gmail.com>
2021-05-05 13:30:32 -0500

    Pagina semifinal, solo falta el quiz.

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const btn3 = document.querySelector("#btn3");
 const btn4 = document.querySelector("#btn4");
 const quizBtns = document.querySelectorAll(".btn-quiz");
 const quizText = document.querySelector("#qText");
+const menu = document.querySelector('.burguer-menu');
+
+
+
+menu.addEventListener('click', () => {
+    document.getElementById('sidebar').classList.toggle('active');
+})
 
 let resultValues =  new Array();
 
